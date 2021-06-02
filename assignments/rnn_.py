6eae678a63f84361eeaee84ded2f9f0fcf0b1a4c
Àlex Costa Sánchez <alex.costasanchez@gmail.com>
2021-06-02 19:05:12 +0200

    Feature: - Gradients finally work

--- assignments/rnn_.py
+++ assignments/rnn_.py
@@ -14,9 +14,7 @@ class RNN:
         self.output_size = output_size
         self.hidden_size = hidden_size
 
-        self.h = np.zeros((self.hidden_size, 1))
-        self.hidden_states = None
-        self.last_predictions = None
+        self.h, self.p = None, None
 
         self.weights = {}
         self.gradients = {}
@@ -66,42 +64,43 @@ class RNN:
             xt = index_2_one_hot(sample, self.output_size)
         return indices_2_chars(output, self.indices_2_char)
 
-    def forward(self, x, h0=None):
+    def forward(self, x, y, h0):
         seq_length = x.shape[1]
-        self.h = h0 if h0 is not None else self.h
-        self.hidden_states = [h0]
-        self.last_predictions = []
+        self.p, self.h = [None] * seq_length, [None] * (seq_length + 1)
+
+        self.h[0] = h0
+        loss = 0
 
         for t in range(seq_length):
-            at = self.weights['W'] @ self.h + self.weights['U'] @ x[:, [t]] + self.weights['b']
-            self.h = np.tanh(at)
-            ot = self.weights['V'] @ self.h + self.weights['c']
-            pt = softmax(ot)  # .squeeze()
+            a = self.weights['W'] @ self.h[t] + self.weights['U'] @ x[:, [t]] + self.weights['b']
+            self.h[t + 1] = np.tanh(a)
+            o = self.weights['V'] @ self.h[t + 1] + self.weights['c']
+            self.p[t] = softmax(o)  # .squeeze()
+            loss -= np.log(y[:, [t]].T @ self.p[t])[0, 0]
 
-            self.hidden_states.append(self.h)
-            self.last_predictions.append(pt)
+        return loss
 
-    def compute_gradients(self, x, y, reg_lambda=0.0):
+    def backward(self, x, y):
         seq_length = x.shape[1]
 
-        dLdo = []
-
-        for t in range(seq_length):
-            dLdo.append(- (y[:, [t]] - self.last_predictions[t]).T)
-            g = dLdo[t]
-            self.gradients['V'] += g.T @ self.hidden_states[t + 1].T
-            self.gradients['c'] += g.T
+        self.gradients = {}
+        for param in self.weights:
+            self.gradients[param] = np.zeros_like(self.weights[param])
 
         dLda = np.zeros((1, self.hidden_size))
+        for t in reversed(range(seq_length)):
+            g = -(y[:, [t]] - self.p[t]).T
+            self.gradients['V'] += g.T @ self.h[t + 1].T
+            self.gradients['c'] += g.T
+            dLdh = g @ self.weights['V'] + dLda @ self.weights['W']
+            dLda = dLdh @ np.diag(1 - self.h[t + 1].squeeze() ** 2)
+            self.gradients['W'] += dLda.T @ self.h[t].T
+            self.gradients['U'] += dLda.T @ x[:, [t]].T
+            self.gradients['b'] += dLda.T
 
-        for t in reversed(range(seq_length - 1)):
-            dLdh = dLdo[t] @ self.weights['V'] + dLda @ self.weights['W']
-            dLda = dLdh @ np.diag(1 - (self.hidden_states[t + 1].squeeze() ** 2))
-            g = dLda
-            self.gradients['W'] += g.T @ self.hidden_states[t + 1].T
-            self.gradients['U'] += g.T @ x[:, [t]].T
-            self.gradients['b'] += g.T
-
+        for param in self.weights:
+            self.gradients[param] = np.clip(self.gradients[param], -5, 5)
+            
         return self.gradients
 
     def update_weights(self, eta):
@@ -109,75 +108,32 @@ class RNN:
             self.m_theta[param] += self.gradients[param] ** 2
             self.weights[param] -= eta / np.sqrt(self.m_theta[param] + epsilon) * self.gradients[param]
 
-    def compute_loss(self, x, y):
-        loss = 0
-        for p in self.last_predictions:
-            loss += - np.sum(np.log(np.sum(y * p, axis=0)))
-        return loss
+    def fit(self, x, y, epochs=2):
+        pass
 
-    def compute_gradients_num(self, x, y, reg_lambda=0.0, h=1e-4):
-        def compute_loss():
-            loss = 0
-            hidden_state = np.zeros(shape=(self.hidden_size, 1))
-            for t in range(x.shape[1]):
-                a = self.weights['W'] @ hidden_state + self.weights['U'] @ x[:, [t]] + self.weights['b']
-                hidden_state = np.tanh(a)
-                ot = self.weights['V'] @ hidden_state + self.weights['c']
-                p = softmax(ot)
-                loss += - np.sum(np.log(np.sum(y * p, axis=0)))
-            return loss
-
+    def compute_gradients_num(self, x, y, h=1e-4):
         num_gradients = {}
         for param in self.weights:
             num_gradients[param] = np.zeros_like(self.weights[param])
 
-        """# Check gradient for W
-        for i in range(len(self.weights['W'])):
-            for j in range(len(self.weights['W'][i])):
-                self.weights['W'][i][j] -= h
-                c1 = compute_loss()
-                self.weights['W'][i][j] += 2 * h
-                c2 = compute_loss()
-                num_gradients['W'][i][j] = (c2 - c1) / (2 * h)
-                self.weights['W'][i][j] -= h
+        for param in ['W', 'U', 'V']:
+            for i in range(len(self.weights[param])):
+                for j in range(len(self.weights[param][i])):
+                    self.weights[param][i][j] -= h
+                    c1 = self.forward(x, y, np.zeros(shape=(10, 1)))
+                    self.weights[param][i][j] += 2 * h
+                    c2 = self.forward(x, y, np.zeros(shape=(10, 1)))
+                    num_gradients[param][i][j] = (c2 - c1) / (2 * h)
+                    self.weights[param][i][j] -= h
 
-        # Check gradient for U
-        for i in range(len(self.weights['U'])):
-            for j in range(len(self.weights['U'][i])):
-                self.weights['U'][i][j] -= h
-                c1 = compute_loss()
-                self.weights['U'][i][j] += 2 * h
-                c2 = compute_loss()
-                num_gradients['U'][i][j] = (c2 - c1) / (2 * h)
-                self.weights['U'][i][j] -= h
-
-        # Check gradient for b
-        for i in range(len(self.weights['b'])):
-            self.weights['b'][i] -= h
-            c1 = compute_loss()
-            self.weights['b'][i] += 2 * h
-            c2 = compute_loss()
-            num_gradients['b'][i] = (c2 - c1) / (2 * h)
-            self.weights['b'][i] -= h"""
-
-        # Check gradient for V
-        for i in range(len(self.weights['V'])):
-            for j in range(len(self.weights['V'][i])):
-                self.weights['V'][i][j] -= h
-                c1 = compute_loss()
-                self.weights['V'][i][j] += 2 * h
-                c2 = compute_loss()
-                num_gradients['V'][i][j] = (c2 - c1) / (2 * h)
-                self.weights['V'][i][j] -= h
-
-        """# Check gradient for c
-        for i in range(len(self.weights['c'])):
-            self.weights['c'][i] -= h
-            c1 = compute_loss()
-            self.weights['c'][i] += 2 * h
-            c2 = compute_loss()
-            num_gradients['c'][i] = (c2 - c1) / (2 * h)
-            self.weights['c'][i] -= h"""
+        for param in ['b', 'c']:
+            for i in range(len(self.weights[param])):
+                self.weights[param][i] -= h
+                c1 = self.forward(x, y, np.zeros(shape=(10, 1)))
+                self.weights[param][i] += 2 * h
+                c2 = self.forward(x, y, np.zeros(shape=(10, 1)))
+                num_gradients[param][i] = (c2 - c1) / (2 * h)
+                self.weights[param][i] -= h
 
         return num_gradients
 
